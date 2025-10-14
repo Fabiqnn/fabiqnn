8406a227f06d2eaacefea6acd1ba689a466a0c90
Fabiqnn <arema20056@gmail.com>
2025-10-15 00:23:24 +0700

    remove " ' "

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,11 +17,11 @@ function Hero() {
             <div id="hero" className="grid items-center w-full max-w-[1400px] h-[80vh] mx-auto px-7 mt-20 font-monsterrat py-20 md:py-30 md:grid-cols-2">
                 <div className="order-2 space-y-5 mx-auto font-inter md:order-1">
                     <div>
-                        {dark ? <ShinyText text="Hello, I'm Fabian" disabled={false} speed={3} className='text-4xl md:text-6xl font-bold' /> : <SplitText text="Hello, I'm Fabian" className="text-4xl md:text-6xl font-bold" textAlign="left" splitType="words"/>}
+                        {dark ? <ShinyText text="Hello, I&apos;m Fabian" disabled={false} speed={3} className='text-4xl md:text-6xl font-bold' /> : <SplitText text="Hello, I&apos;m Fabian" className="text-4xl md:text-6xl font-bold" textAlign="left" splitType="words"/>}
                         {dark ? <ShinyText text="Junior Web Developer" disabled={false} speed={3} className='text-2xl md:text-3xl font-bold'/> : <div className="text-2xl md:text-3xl font-bold">Web Developer</div>}
                     </div>
                     <p className="text-base">Undergraduate student majoring in Information Technology at POLINEMA. <br className="hidden custom-br1:block"/>
-                        I'm passionate about building useful and user-friendly web applications, <br className="hidden custom-br1:block"/> and I'm always eager to improve and grow in the tech industry.</p>
+                        I&apos;m passionate about building useful and user-friendly web applications, <br className="hidden custom-br1:block"/> and I&apos;m always eager to improve and grow in the tech industry.</p>
                     <p className="text-xl font-semibold">I can help you with:</p>
                     <ul className="list-disc list-inside text-md">
                         <li>Laravel Development</li>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,7 +5,7 @@ function Project() {
         <div id="project" className="items-center w-full max-w-[1400px] h-full px-7 font-monsterrat mx-auto mt-40 mb-10 md:py-20 md:mt-0 md:mb-20">
             <div className="md:px-18">
                 <h1 className="text-2xl md:text-3xl font-bold">My Project</h1>
-                <h3 className="text-base" >Here are some project i've worked on!</h3>
+                <h3 className="text-base" >Here are some project i&apos;ve worked on!</h3>
             </div>
             <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 lg:scale-90 gap-8">
                 <Card img="assets/fixit.png" title="FixIT" text="Late 2024" link="./projects/fixit"/>
